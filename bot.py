68373c5500c5b37fdc5fb802429249370a62eeec
Katerina Gakhova <gakhovakaterina16@gmail.com>
2020-07-01 11:13:27 +0300

    deleted .vscode and bot.log

--- bot.py
+++ bot.py
@@ -1,22 +1,25 @@
 import logging
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import settings
 
 logging.basicConfig(filename='bot.log', level=logging.INFO)
+
 
 def greet_user(update, context):
     print('Вызван /start')
     update.message.reply_text('Привет, пользователь!')
 
+
 def main():
     mybot = Updater(settings.API_KEY, use_context=True)
-    
+
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
-    
-    logging.info('Бот стартовал')
+
+    logging.info('The bot has started!')
     mybot.start_polling()
     mybot.idle()
 
-if __name__ == 'main':
+
+if __name__ == '__main__':
     main()
