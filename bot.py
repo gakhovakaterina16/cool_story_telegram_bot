34846657a3f021776ccb64e3d03b49cbcedd4632
Katerina Gakhova <gakhovakaterina16@gmail.com>
2020-07-01 12:21:14 +0300

    functions: talk_to_me, guess number@

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import settings
+from random import randint
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
 
 logging.basicConfig(filename='bot.log', level=logging.INFO)
 
@@ -10,11 +12,44 @@ def greet_user(update, context):
     update.message.reply_text('Привет, пользователь!')
 
 
+def talk_to_me(update, context):
+    user_text = update.message.text
+    print(user_text)
+    update.message.reply_text(user_text)
+
+
+def guess_number(update, context):
+    if context.args:
+        try:
+            user_number = int(context.args[0])
+            message = play_random_numbers(user_number)
+        except (TypeError, ValueError):
+            message = 'Введите целое число'
+    else:
+        message = 'Введите целое число'
+    update.message.reply_text(message)
+
+
+def play_random_numbers(user_number):
+    bot_number = randint(user_number-10, user_number+10)
+    if user_number > bot_number:
+        message = f'Ты загадал {user_number}, я загадал {bot_number}.\n \
+            Ты выиграл!'
+    elif user_number < bot_number:
+        message = f'Ты загадал {user_number}, я загадал {bot_number}. \n \
+        Я выиграл!'
+    else:
+        message = f'Ты загадал {user_number}, я загадал {bot_number}. Ничья!'
+    return message
+
+
 def main():
     mybot = Updater(settings.API_KEY, use_context=True)
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(CommandHandler('guess', guess_number))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     logging.info('The bot has started!')
     mybot.start_polling()
